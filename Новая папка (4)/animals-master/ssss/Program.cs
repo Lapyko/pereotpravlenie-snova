cce06591189bbc89c3c9e8c39869bd09586129c3
Lapyko <113054648+Lapyko@users.noreply.github.com>
2022-09-26 15:50:03 +0300

    Add files via upload

---
+++ Новая папка (4)/animals-master/ssss/Program.cs
@@ -0,0 +1,23 @@
+﻿using ssss;
+
+namespace ssss
+ {
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Veterinar veterinar = new Veterinar();
+            Animal[] animal = new Animal[3];
+            animal[0] = new Dog("спасает", "корм", "приют");
+            animal[1] = new Cat("линяет", "пельмени", "коммунар");
+            animal[2] = new Horse("орет", "сено", "ставрово");
+            
+            for (int i = 0; i < animal.Length; i++)
+            {
+                veterinar.treatAnimal(animal[i]);
+            }
+
+
+        }  
+    }
+}
